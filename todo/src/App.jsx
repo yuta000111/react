0f455edc6454f7659c186109464b1efd72a32e2f
yuta000111 <yuuta0114@gmail.com>
2021-11-18 18:31:53 +0900

    ステート＆コンテキスト

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -2,21 +2,29 @@ import React, { createContext, useState } from 'react';
 import TodoList from './compornent/todoList';
 import UserInput from './compornent/userInput';
 
-export const allList = createContext({
-  id: '',
-  task: '',
-  status: false,
-  remove: false,
-});
-
-const [tasks, setTasks] = useState(allList);
+export const allList = createContext([
+  {
+    id: '',
+    task: '',
+    status: false,
+    remove: false,
+  },
+]);
 
 function App() {
+  const [tasks, setTasks] = useState(allList);
+
+  const addTasks = ({ task, status, remove }) => {
+    setTasks([
+      ...tasks,
+      { id: tasks.length + 1, task, status, remove },
+    ]);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <h1>todo app</h1>
-        <allList.Provider>
+        <allList.Provider value={{ tasks, addTasks }}>
           <TodoList></TodoList>
           <UserInput></UserInput>
         </allList.Provider>
--- todo/src/compornent/todoList.jsx
+++ todo/src/compornent/todoList.jsx
@@ -1,23 +1,26 @@
 import React, { useContext } from 'react';
+// import Todo from './todo';
 import { allList } from '../App';
-import Todo from './todo';
 
 function TodoList() {
-  const { newLists } = useContext(allList);
-  if (!newLists.length) return;
-  <div className="TodoList">
-    <h2>タスク一覧</h2>
-  </div>;
-  return (
-    <div className="TodoList">
-      <h2>タスク一覧</h2>
-      <ul>
-        {newLists.map((newList) => {
-          <Todo key={newList.id} />;
-        })}
-      </ul>
-    </div>
-  );
+  const { tasks } = useContext(allList);
+  console.log(tasks);
+  return <p>テスト</p>;
+  // if (!tasks.length) return;
+  // <div className="TodoList">
+  //   <h2>タスク一覧</h2>
+  //   <p> タスクはまだありません。</p>
+  // </div>;
+  // return (
+  //   <div className="TodoList">
+  //     <h2>タスク一覧</h2>
+  //     <ul>
+  //       {tasks.map((newList) => {
+  //         <Todo key={newList.id} />;
+  //       })}
+  //     </ul>
+  //   </div>
+  // );
 }
 
 export default TodoList;
