e506a45727cf46ecd0a36c9f51ac9fc3a1839042
yuta000111 <yuuta0114@gmail.com>
2021-11-19 17:13:19 +0900

    タスク追加ロジック追加( totyuu’ ） ;

--- todo/src/compornent/userInput.jsx
+++ todo/src/compornent/userInput.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { allList } from '../App';
 
 function userInput() {
+  const { addTasks } = useContext(allList);
+  const [task, setTask] = useState('');
+  const [status, setStatus] = useState(false);
+  const [remove, setRemove] = useState(false);
+  const submitTask = (e) => {
+    e.preventDefault();
+    addTasks(task, status, remove);
+  };
+
   return (
     <div className="userInput">
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" />
+      <form onSubmit={submitTask}>
+        <input
+          type="text"
+          value={task}
+          onChange={(event) => {
+            setTask(event.target.value);
+          }}
+        />
+        <input
+          type="checkbox"
+          value={status}
+          onChange={(event) => {
+            setStatus(event.target.value);
+          }}
+        />
+        <input
+          type="checkbox"
+          value={remove}
+          onChange={(event) => {
+            setRemove(event.target.value);
+          }}
+        />
         <input type="button" value="追加" />
       </form>
     </div>
