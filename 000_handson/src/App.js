73e685e3821a7ac100152b36e593fa28a43a2b45
yuta000111 <yuuta0114@gmail.com>
2021-11-27 16:57:28 +0900

    reactの勉強

--- 000_handson/src/App.js
+++ 000_handson/src/App.js
@@ -1,34 +1,32 @@
 import React from 'react';
-import { FixedSizeList } from 'react-window';
-import faker from 'faker';
+import Fetch from './Fetch';
 
-const bigList = [...Array(5000)].map(() => ({
-  name: faker.name.findName(),
-  email: faker.internet.email(),
-  avatar: faker.internet.avatar(),
-}));
+function GitHubUser({ login }) {
+  return (
+    <Fetch
+      uri={`https://api.github.com/users/${login}`}
+      renderSuccess={UserDetails}
+    />
+  );
+}
 
-export default function App() {
-  const renderRow = ({ index, style }) => (
-    <div style={{ ...style, ...{ display: 'flex' } }}>
+function UserDetails({ data }) {
+  return (
+    <div className="githubUser">
       <img
-        src={bigList[index].avatar}
-        alt={bigList[index].name}
-        width={50}
+        src={data.avatar_url}
+        alt={data.login}
+        style={{ width: 200 }}
       />
-      <p>
-        {bigList[index].name} - {bigList[index].email}
-      </p>
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
     </div>
   );
+}
 
-  return (
-    <FixedSizeList
-      height={window.innerHeight}
-      width={window.innerWidth - 20}
-      itemCount={bigList.length}
-      itemSize={50}>
-      {renderRow}
-    </FixedSizeList>
-  );
+export default function App() {
+  return <GitHubUser login="moonhighway" />;
 }
