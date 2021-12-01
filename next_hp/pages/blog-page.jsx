5e4a90355f1ae55f6003d081b47161727bdbf57e
yuta000111 <yuuta0114@gmail.com>
2021-12-01 21:09:44 +0900

    getgetStaticPropsについて学ぶ

--- next_hp/pages/blog-page.jsx
+++ next_hp/pages/blog-page.jsx
@@ -1,6 +1,25 @@
 import Layout from '../component/Layout';
-export const Blog = () => {
-  return <Layout title="blog">Blog page</Layout>;
+import { getAllPostsData } from '../lib/posts';
+import Post from '../component/post';
+
+export const Blog = ({ posts }) => {
+  return (
+    <Layout title="Blog">
+      <ul className="m-10">
+        {posts &&
+          posts.map((post) => (
+            <Post key={post.id} post={post} />
+          ))}
+      </ul>
+    </Layout>
+  );
 };
 
 export default Blog;
+
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts },
+  };
+}
